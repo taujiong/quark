0ca4418978f155efb167438cf80cb5a20fe1ee77
taujiong <taujiong@outlook.com>
2022-04-10 19:45:19 +0800

    chore: 添加检查环境变量一致性的 git hook

---
+++ scripts/check-env.js
@@ -0,0 +1,48 @@
+const { existsSync, readFileSync } = require('fs')
+const { join } = require('path')
+
+const workingDir = process.cwd()
+const envPath = join(workingDir, '.env')
+const envSamplePath = join(workingDir, '.env.sample')
+
+if (!existsSync(envPath) || !existsSync(envSamplePath)) {
+  console.log(
+    '.env or .env.sample not exists in the workspace, check is aborted.'
+  )
+  process.exit()
+}
+
+const envItems = parseEnvItemsFromFile(envPath)
+const envSampleItems = parseEnvItemsFromFile(envSamplePath)
+
+if (envItems.length !== envSampleItems.length) {
+  logError()
+  process.exit()
+}
+
+for (const [index, item] of envItems.entries()) {
+  if (item !== envSampleItems[index]) {
+    logError()
+    break
+  }
+}
+
+process.exit()
+
+/**
+ * @param {string} filePath
+ */
+function parseEnvItemsFromFile(filePath) {
+  return readFileSync(filePath, 'utf8')
+    .split('\n') // get each line
+    .filter((line) => line.indexOf('=') !== -1) // valid env item
+    .map((line) => line.split('=')[0]) // get key
+    .sort() // make it in order
+}
+
+function logError() {
+  console.log(
+    '\n\x1B[31mError: Items in .env.sample not consistent with those in .env\x1B[0m\n'
+  )
+  process.exitCode = 1
+}
